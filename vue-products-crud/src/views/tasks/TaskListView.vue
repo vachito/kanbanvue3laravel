8006a98cd656bb987dd373d309a36db9f12ccec6
Rigoberto Xocoxic <vacho19@gmail.com>
2026-03-26 15:05:59 -0600

    kanban

--- vue-products-crud/src/views/tasks/TaskListView.vue
+++ vue-products-crud/src/views/tasks/TaskListView.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-    import { onMounted } from 'vue';
+    import { onMounted, ref } from 'vue';
     import { useTaskStore } from '../../stores/taskStore';
+    import { VueDraggableNext } from 'vue-draggable-next';
+    import type { Task } from '../../types/productTypes';
 
     const store = useTaskStore()
+    const todo = ref<Task[]>([])
+    const done = ref<Task[]>([])
 
     onMounted(async()=>{
         await store.getAllTasks()
-        console.log(store.tasks)
     })
+    
+    const splitTasks = () =>{
+        todo.value = store.tasks.filter(t => !t.is_complete)
+        done.value = store.tasks.filter(t => t.is_complete)
+    }
+    splitTasks()
+
+    const updateTaskStatus = async (task:Task, status:boolean) =>{
+        
+    }
 </script>
 
 <template>
